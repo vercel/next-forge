5899454081bdb20c1965ec06da8bc622b5b390b3
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-06 15:22:09 -0700

    Migrate to Radix monopackage

--- packages/design-system/components/ui/alert-dialog.tsx
+++ packages/design-system/components/ui/alert-dialog.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
+import { AlertDialog as AlertDialogPrimitive } from "radix-ui"
 
 import { cn } from "@repo/design-system/lib/utils"
 import { buttonVariants } from "@repo/design-system/components/ui/button"
