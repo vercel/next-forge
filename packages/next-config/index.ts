d83937b68c4d8bddd030fc6365f48c211e52374f
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-06 16:46:24 -0700

    Resolves #617

--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -1,18 +1,13 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-// @ts-expect-error No declaration file
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
-import type { NextConfig } from 'next';
-
-const otelRegex = /@opentelemetry\/instrumentation/;
+import withBundleAnalyzer from "@next/bundle-analyzer";
+import type { NextConfig } from "next";
 
 export const config: NextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
+        protocol: "https",
+        hostname: "img.clerk.com",
       },
     ],
   },
@@ -21,29 +16,18 @@ export const config: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*',
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
       },
       {
-        source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*',
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
       },
       {
-        source: '/ingest/decide',
-        destination: 'https://us.i.posthog.com/decide',
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
       },
     ];
-  },
-
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.plugins = config.plugins || [];
-      config.plugins.push(new PrismaPlugin());
-    }
-
-    config.ignoreWarnings = [{ module: otelRegex }];
-
-    return config;
   },
 
   // This is required to support PostHog trailing slash API requests
