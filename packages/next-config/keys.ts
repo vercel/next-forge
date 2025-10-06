286c8a3ccf9c4590b109ad473183b75cdd545647
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-06 16:13:33 -0700

    Build fixes

--- packages/next-config/keys.ts
+++ packages/next-config/keys.ts
@@ -1,15 +1,20 @@
-import { vercel } from '@t3-oss/env-core/presets-zod';
 import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 export const keys = () =>
   createEnv({
-    extends: [vercel()],
     server: {
       ANALYZE: z.string().optional(),
 
       // Added by Vercel
       NEXT_RUNTIME: z.enum(['nodejs', 'edge']).optional(),
+
+      // Vercel environment variables
+      VERCEL: z.string().optional(),
+      VERCEL_ENV: z.enum(['development', 'preview', 'production']).optional(),
+      VERCEL_URL: z.string().optional(),
+      VERCEL_REGION: z.string().optional(),
+      VERCEL_PROJECT_PRODUCTION_URL: z.string().optional(),
     },
     client: {
       NEXT_PUBLIC_APP_URL: z.string().url(),
@@ -20,6 +25,11 @@ export const keys = () =>
     runtimeEnv: {
       ANALYZE: process.env.ANALYZE,
       NEXT_RUNTIME: process.env.NEXT_RUNTIME,
+      VERCEL: process.env.VERCEL,
+      VERCEL_ENV: process.env.VERCEL_ENV,
+      VERCEL_URL: process.env.VERCEL_URL,
+      VERCEL_REGION: process.env.VERCEL_REGION,
+      VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
       NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
       NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
       NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
