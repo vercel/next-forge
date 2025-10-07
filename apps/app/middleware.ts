48486192a015169629a613ff6c45013b3288b5ef
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-06 17:45:55 -0700

    Run middleware on node

--- apps/app/middleware.ts
+++ apps/app/middleware.ts
@@ -22,4 +22,5 @@ export const config = {
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
+  runtime: "nodejs",
 };
--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -18,6 +18,7 @@ export const config = {
   // matcher tells Next.js which routes to run the middleware on. This runs the
   // middleware on all routes except for static assets and Posthog ingest
   matcher: ["/((?!_next/static|_next/image|ingest|favicon.ico).*)"],
+  runtime: "nodejs",
 };
 
 const securityHeaders = env.FLAGS_SECRET
