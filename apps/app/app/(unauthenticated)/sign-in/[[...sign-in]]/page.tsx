379876c9c739015b8da1a4b1489a6e186c300097
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-06 19:15:28 -0700

    Cleanup auth pages

--- apps/app/app/(unauthenticated)/sign-in/[[...sign-in]]/page.tsx
+++ apps/app/app/(unauthenticated)/sign-in/[[...sign-in]]/page.tsx
@@ -10,14 +10,6 @@ const SignIn = dynamic(() =>
 
 export const metadata: Metadata = createMetadata({ title, description });
 
-const SignInPage = () => (
-  <>
-    <div className="flex flex-col space-y-2 text-center">
-      <h1 className="font-semibold text-2xl tracking-tight">{title}</h1>
-      <p className="text-muted-foreground text-sm">{description}</p>
-    </div>
-    <SignIn />
-  </>
-);
+const SignInPage = () => <SignIn />;
 
 export default SignInPage;
--- apps/app/app/(unauthenticated)/sign-up/[[...sign-up]]/page.tsx
+++ apps/app/app/(unauthenticated)/sign-up/[[...sign-up]]/page.tsx
@@ -10,14 +10,6 @@ const SignUp = dynamic(() =>
 
 export const metadata: Metadata = createMetadata({ title, description });
 
-const SignUpPage = () => (
-  <>
-    <div className="flex flex-col space-y-2 text-center">
-      <h1 className="font-semibold text-2xl tracking-tight">{title}</h1>
-      <p className="text-muted-foreground text-sm">{description}</p>
-    </div>
-    <SignUp />
-  </>
-);
+const SignUpPage = () => <SignUp />;
 
 export default SignUpPage;
