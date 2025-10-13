ef8da5406d48a400e3a64363c35e3866a6b71ebb
Kuizuo <hi@kuizuo.cn>
2025-10-14 03:45:22 +0800

    Update Storybook to the latest version
    - Bump Storybook and related addons to version 9.1.10.
    - Remove deprecated `@storybook/addon-actions` and update imports in story files to use the new action handling.
    - Update `@chromatic-com/storybook` to version 4.1.1.
    - Clean up Storybook configuration by removing unused addons.

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -17,10 +17,8 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-themes"),
   ],
   framework: {
