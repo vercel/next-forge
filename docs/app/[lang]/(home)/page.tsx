ca7baf8a502643b1ff956f8a380e76a82e678f27
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-06 09:33:30 -0800

    Update homepage

--- docs/app/[lang]/(home)/page.tsx
+++ docs/app/[lang]/(home)/page.tsx
@@ -2,26 +2,17 @@ import { Apps } from "./components/apps";
 import { CallToAction } from "./components/cta";
 import { Features } from "./components/features";
 import { Hero } from "./components/hero";
-import { OpenSource } from "./components/open-source";
-import { Review } from "./components/review";
 import { Social } from "./components/social";
 
 const Home = () => (
-  <main className="container mx-auto divide-y border-x px-0">
+  <main className="container mx-auto px-0 pb-24">
     <Hero />
-    <div className="grid divide-y sm:grid-cols-3 sm:divide-x sm:divide-y-0">
-      <div className="sm:col-span-2">
-        <Review />
-      </div>
-      <div className="sm:col-span-1">
-        <OpenSource />
-      </div>
+    <div className="divide-y border-x border-y">
+      <Apps />
+      <Features />
+      <Social />
+      <CallToAction />
     </div>
-    <div className="h-8 bg-dashed" />
-    <Apps />
-    <Features />
-    <Social />
-    <CallToAction />
   </main>
 );
 
