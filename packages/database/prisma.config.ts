3ccb143182c404994042f99ca41c347b3212fb92
Dmytro Pletenskyi <32138582+ph1losof@users.noreply.github.com>
2025-12-06 18:25:58 +0100

    fix: update prisma to 7.0.0 and use prisma-client generator (#663)
    * fix: update prisma to 7.0.0 and use esm compatible prisma-client generator
    
    * docs(migrations): update planetscale migration to use newest prisma.schema format

---
+++ packages/database/prisma.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "prisma/config";
+import { keys } from "./keys";
+import "dotenv/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: keys().DATABASE_URL,
+  },
+});
