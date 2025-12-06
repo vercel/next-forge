7c149ab5ed085a4ff5865b8c5bf94ca6de0a3af1
Hayden Bleasel <hello@haydenbleasel.com>
2025-12-06 09:48:15 -0800

    Update page.tsx

--- docs/app/[lang]/(home)/page.tsx
+++ docs/app/[lang]/(home)/page.tsx
@@ -1,8 +1,15 @@
+import type { Metadata } from "next";
 import { Apps } from "./components/apps";
 import { CallToAction } from "./components/cta";
 import { Features } from "./components/features";
 import { Hero } from "./components/hero";
 import { Social } from "./components/social";
+
+export const metadata: Metadata = {
+  title: "Production-grade Turborepo template for Next.js apps | next-forge",
+  description:
+    "A monorepo template designed to have everything you need to build your new SaaS app as thoroughly as possible. Free and open source, forever.",
+};
 
 const Home = () => (
   <main className="container mx-auto px-0 pb-24">
